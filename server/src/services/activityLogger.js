fec45d679f2133df0454a24c2696919d9da201e1
Aditya-k-11 <adityakanojia.ad@gmail.com>
2026-08-14 17:50:53 +0530

    fix: named logger export in activityLogger

--- server/src/services/activityLogger.js
+++ server/src/services/activityLogger.js
@@ -1,5 +1,5 @@
 import ActivityLog from '../models/ActivityLog.js';
-import logger from '../config/logger.js';
+import { logger } from '../config/logger.js';
 
 /**
  * Logs an activity entry asynchronously.
